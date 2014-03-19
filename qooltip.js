8e5a3127f3ac96c830c2401bcce25e9299feb1c1
Kārlis Andersons <karlisandersons89@gmail.com>
2014-03-19 16:25:20 +0200

    Better variable naming

--- qooltip.js
+++ qooltip.js
@@ -39,11 +39,11 @@
 
     var winWidth,
         winHeight,
-        addEventListener = 'addEventListener', // Save some bytes
-        dataAttrName = 'data-qooltip',         //
-        style = 'style',                       //
-        target = 'target',                     //
-        attributes = 'attributes',             //
+        addEventListenerString = 'addEventListener', // Save some bytes
+        dataAttrNameString = 'data-qooltip',         //
+        styleString = 'style',                       //
+        targetString = 'target',                     //
+        attributesString = 'attributes',             //
         tip = document.createElement('div'),
         tipWidth,
         tipHeight,
@@ -52,51 +52,51 @@
         mouseX, // Mouse coordinates (ex e.clientX)
         mouseY; //
 
-    document[addEventListener]('DOMContentLoaded', function()
+    document[addEventListenerString]('DOMContentLoaded', function()
     {
         tip.id = 'qooltip';
 
         // js-styles
         // Uncomment these lines if you want to add style with js instead
-        // tip[style].background = 'rgba(0, 0, 0, 0.8)';
-        // tip[style].borderRadius = '4px';
-        // tip[style].color = '#fff';
-        // tip[style].left = '-99%';
-        // tip[style].font = '12px/1 sans-serif';
-        // tip[style].position = 'fixed';
-        // tip[style].padding = '5px 10px';
-        // tip[style].whiteSpace = 'nowrap';
-        // tip[style].zIndex = 9999;
+        // tip[styleString].background = 'rgba(0, 0, 0, 0.8)';
+        // tip[styleString].borderRadius = '4px';
+        // tip[styleString].color = '#fff';
+        // tip[styleString].left = '-99%';
+        // tip[styleString].font = '12px/1 sans-serif';
+        // tip[styleString].position = 'fixed';
+        // tip[styleString].padding = '5px 10px';
+        // tip[styleString].whiteSpace = 'nowrap';
+        // tip[styleString].zIndex = 9999;
 
         document.body.appendChild(tip);
     });
 
-    document[addEventListener]('mouseover', function(e)
+    document[addEventListenerString]('mouseover', function(e)
     {
-        if (e[target][attributes][dataAttrName]) {
+        if (e[targetString][attributesString][dataAttrNameString]) {
             mouseOverHandler(e);
         }
     });
 
-    document[addEventListener]('mousemove', function(e)
+    document[addEventListenerString]('mousemove', function(e)
     {
-        if (e[target][attributes][dataAttrName]) {
+        if (e[targetString][attributesString][dataAttrNameString]) {
             mouseMoveHandler(e);
         }
     });
 
-    document[addEventListener]('mousedown', function(e)
+    document[addEventListenerString]('mousedown', function(e)
     {
-        // if (e[target][attributes][dataAttrName]) {
+        // if (e[targetString][attributesString][dataAttrNameString]) {
         //     hideTip();
         // }
 
         hideTip();
     });
 
-    document[addEventListener]('mouseout', function(e)
+    document[addEventListenerString]('mouseout', function(e)
     {
-        // if (e[target][attributes][dataAttrName]) {
+        // if (e[targetString][attributesString][dataAttrNameString]) {
         //     hideTip();
         // }
 
@@ -110,7 +110,7 @@
         e.stopPropagation();
 
         // Populate tooltip with some content
-        tip.innerHTML = e[target].getAttribute(dataAttrName);
+        tip.innerHTML = e[targetString].getAttribute(dataAttrNameString);
 
         // Get width and height of tooltip
         tipWidth = tip.offsetWidth;
@@ -127,18 +127,18 @@
         mouseY = e.clientY;
 
         // Set default position
-        tip[style].top = mouseY + tipOffsetY + 'px';
-        tip[style].left = mouseX + tipOffsetX + 'px';
+        tip[styleString].top = mouseY + tipOffsetY + 'px';
+        tip[styleString].left = mouseX + tipOffsetX + 'px';
 
         // Check if tooltip is not going out of the horizontal window bonds
         if ((mouseX + tipWidth + tipOffsetX) > winWidth) {
             // - 6 so offset looks as same as on the right side (Mac Chrome 33.0.1750.152)
-            tip[style].left = mouseX - tipWidth - (tipOffsetX - 6) + 'px';
+            tip[styleString].left = mouseX - tipWidth - (tipOffsetX - 6) + 'px';
         }
 
         // Check if tooltip is not going out of the vertical window bonds
         if ((mouseY + tipHeight + tipOffsetY) > winHeight) {
-            tip[style].top = mouseY - tipHeight - tipOffsetY + 'px';
+            tip[styleString].top = mouseY - tipHeight - tipOffsetY + 'px';
         }
 
         // Sometimes when mouse is moved slowly out of the window mouseout event
@@ -150,6 +150,6 @@
 
     function hideTip()
     {
-        tip[style].top = '-99%'
+        tip[styleString].top = '-99%';
     }
 })(window, document);
